faff41ed524d03245604136147650bc99989950f
hyperbanner <131620724+HyperBanner@users.noreply.github.com>
2024-06-12 01:29:15 +0200

    Hover effect added

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-function createGrid(n) {
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
+function main(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
       let pixel = document.createElement("div");
       pixel.setAttribute("class", "pixel");
       document.querySelector(".container").appendChild(pixel);
+
+      pixel.addEventListener("mouseenter", () => {
+        pixel.setAttribute("style", "background-color: black");
+      });
     }
   }
 }
 
-createGrid(16);
+main(16);
