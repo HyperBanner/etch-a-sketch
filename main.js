9af99ec998c6c87e1ea26269628760ae1c045fac
hyperbanner <131620724+HyperBanner@users.noreply.github.com>
2024-06-16 18:01:51 +0200

    Limit put on grid size

--- main.js
+++ main.js
@@ -2,21 +2,26 @@ const button = document.querySelector("button");
 const container = document.querySelector(".container");
 
 function generateGrid(n) {
-  n = +prompt("Grid Size:");
+  n = Math.floor(
+    +prompt("Type in grid size: (Only works with positive integers, max 100)")
+  );
+  if (n > 100) {
+    alert("Max size exceeded.");
+  } else {
+    for (let rows = 0; rows < n; rows++) {
+      let row = document.createElement("div");
+      row.className = "row";
+      row.id = "row" + rows;
 
-  for (let rows = 0; rows < n; rows++) {
-    let row = document.createElement("div");
-    row.className = "row";
-    row.id = "row" + rows;
-
-    for (let cells = 0; cells < n; cells++) {
-      let cell = document.createElement("div");
-      cell.className = "cell";
-      cell.id = "cell" + rows + cells;
-      hover(cell);
-      row.appendChild(cell);
+      for (let cells = 0; cells < n; cells++) {
+        let cell = document.createElement("div");
+        cell.className = "cell";
+        cell.id = "cell" + rows + cells;
+        hover(cell);
+        row.appendChild(cell);
+      }
+      container.appendChild(row);
     }
-    container.appendChild(row);
   }
 }
 
