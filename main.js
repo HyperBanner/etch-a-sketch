e50e228809efa8e3197655ad3c17a4dcb91470de
hyperbanner <131620724+HyperBanner@users.noreply.github.com>
2024-06-16 17:57:05 +0200

    Hover effect added

--- main.js
+++ main.js
@@ -7,14 +7,23 @@ function generateGrid(n) {
   for (let rows = 0; rows < n; rows++) {
     let row = document.createElement("div");
     row.className = "row";
+    row.id = "row" + rows;
 
     for (let cells = 0; cells < n; cells++) {
       let cell = document.createElement("div");
       cell.className = "cell";
+      cell.id = "cell" + rows + cells;
+      hover(cell);
       row.appendChild(cell);
     }
     container.appendChild(row);
   }
+}
+
+function hover(cell) {
+  cell.addEventListener("mouseover", function () {
+    cell.style.backgroundColor = "black";
+  });
 }
 
 button.addEventListener("click", generateGrid);
