7d85ae42e2661effa69675e7b727b15c75d0bd3f
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-02-13 00:17:38 +0100

    Project Done (for now)
    The project is finally at a point which I feel satisfied with.
    Setting a grid size works through a button, and I also added
    a lower bound for the grid size, so it can't end up looking
    stupid by inputting a really small grid size.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const box = document.querySelector(".container");
+const setGridBtn = document.querySelector(".set-grid");
+let newGridSize = 0;
 
 function createGrid(n) {
   doRows(n);
@@ -9,7 +11,7 @@ function createGrid(n) {
 
 function doRows(n) {
   for (let i = 0; i < n; i++) {
-    const row = document.createElement("div");
+    let row = document.createElement("div");
     row.setAttribute("class", "row");
     box.appendChild(row);
   }
@@ -21,9 +23,50 @@ function doCells(n) {
     for (let j = 0; j < n; j++) {
       let cell = document.createElement("div");
       cell.setAttribute("class", "cell");
+      cell.addEventListener("mouseenter", () => {
+        cell.style.backgroundColor = "black";
+      });
       rows[j].appendChild(cell);
     }
   }
 }
+
+function clearGrid() {
+  while (box.childElementCount > 0) {
+    box.firstChild.remove();
+  }
+}
+
+function getInput() {
+  newGridSize = prompt(
+    "Please enter number for the new grid size (min: 16; max: 100):",
+  );
+  if (newGridSize === null) {
+    alert("Please input something.");
+    getInput();
+  } else if (newGridSize === "") {
+    alert("Please input something.");
+    getInput();
+  } else if (isNaN(newGridSize)) {
+    alert("Input must be an integer.");
+    getInput();
+  } else if (newGridSize % 1 !== 0) {
+    alert("Input must be an integer.");
+    getInput();
+  } else if (newGridSize > 100) {
+    alert("Input must not be larger than 100.");
+    getInput();
+  } else if (newGridSize < 16) {
+    alert("Input must not be smaller than 16.");
+    getInput();
+  }
+  return newGridSize;
+}
+
+setGridBtn.addEventListener("click", () => {
+  clearGrid();
+  let n = getInput();
+  createGrid(n);
+});
 
 createGrid(16);
