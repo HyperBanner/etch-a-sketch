556da33d4b35af661a36c0f1b5ca45bbe4dc4359
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-02-12 22:57:23 +0100

    Layout Done
    JS logic for making grid appear correctly
    figured out for variable grid sizes. CSS
    adjusted accordingly.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const box = document.querySelector(".container");
+
+function createGrid(n) {
+  doRows(n);
+  doCells(n);
+}
+
+function doRows(n) {
+  for (let i = 0; i < n; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    box.appendChild(row);
+  }
+}
+
+function doCells(n) {
+  let rows = document.querySelectorAll(".row");
+  for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < n; j++) {
+      let cell = document.createElement("div");
+      cell.setAttribute("class", "cell");
+      rows[j].appendChild(cell);
+    }
+  }
+}
+
+createGrid(16);
